5dc47aaa5b03b471a8401dcd7d7607035896b7e8
Austin Beck <austin.beck@aboutobjects.com>
2023-05-14 11:39:25 -0500

    Removing commented code

--- Pokedex/Model/Pokemon.swift
+++ Pokedex/Model/Pokemon.swift
@@ -69,13 +69,3 @@ struct MoveLearnMethod: Codable {
     let url: String
 }
 
-//var testPokemon = Pokemon(name: "test", url: "test.com")
-//var testResults = Results(count: 1, next: "none.com", results: [testPokemon])
-//var testForms = Forms(name: "test name", url: "test.com")
-//var testSinglePokemon = SinglePokemon(id: 1, height: 1, base_experience: 1, forms: [testForms], sprites: testSprites, moves: [testMoves])
-//var testSprites = Sprites(front_default: "", front_shiny: "")
-//var testMoveLearnMethod = MoveLearnMethod(name: "", url: "")
-//var testVersionGroupDetails = VersionGroupDetails(level_learned_at: 1, move_learn_method: testMoveLearnMethod)
-//var testMove = Move(name: "", url: "")
-//var testMoves = Moves(move: testMove, version_group_details: [testVersionGroupDetails])
-
